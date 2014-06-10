56ae92ad30eb8ba221e01363bd3959762293bcc8
Michael Allwright <michael.allwright@upb.de>
2014-06-10 11:12:49 +0200

    Removed main.cpp and merged contents into Firmware.cpp, cleaned up Makefile

--- firmware/Firmware.cpp
+++ firmware/Firmware.cpp
@@ -1,7 +1,17 @@
 #include "Firmware.h"
 
-/* static initialization of the timer */
+/* Instantiate the firmware */
+Firmware firmware;
+
+/* Static initialization of the timer */
 Timer Firmware::timer;
 
-/* static initialization of the serial port */
+/* Static initialization of the serial port */
 HardwareSerial Firmware::serial(&rx_buffer, &tx_buffer, &UBRR0H, &UBRR0L, &UCSR0A, &UCSR0B, &UCSR0C, &UDR0, RXEN0, TXEN0, RXCIE0, UDRIE0, U2X0);
+
+/* Run the firmware */
+int main(void)
+{
+   return firmware.exec();
+}
+
